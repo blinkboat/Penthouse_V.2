37bdfabce700002d6e48a6d846d7a686e617c821
13oat <mrblinkboat@gmail.com>
2019-11-03 14:00:29 +0700

    Add project files.

---
+++ Penthouse/Controllers/LawController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Penthouse.Controllers
+{
+    public class LawController : Controller
+    {
+        // GET: Law
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult _THAI_REAL_ESTATE_LAWS()
+        {
+
+            return View();
+        }
+        public ActionResult _THAI_TAX_LAWS()
+        {
+
+            return View();
+        }
+    }
+}
---
+++ Penthouse/Controllers/ProjectDetailsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Penthouse.Controllers
+{
+    public class ProjectDetailsController : Controller
+    {
+        // GET: ProjectDetails
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ROYALTHAIVILLAS()
+        {
+            return View();
+        }
+        public ActionResult KHUNBYYOU()
+        {
+            return View();
+        }
+        public ActionResult Wireless()
+        {
+            return View(); 
+        }
+        public ActionResult MONUMENTTHONGLO()
+        {
+            return View();
+        }
+        public ActionResult BEATNIQ()
+        {
+            return View();
+        }
+        public ActionResult SCOPELANGSUAN()
+        {
+            return View(); 
+        }
+        public ActionResult HYDESUKHUMVIT()
+        {
+            return View();
+        }
+        public ActionResult SALADAENGONE()
+        {
+            return View();
+        }
+    }
+}
